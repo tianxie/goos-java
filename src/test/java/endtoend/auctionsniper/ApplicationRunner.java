0d97748170324f67e6aa3c2889aaeffa3c39603d
xietian <txie@live.cn>
2020-08-20 20:52:13 +0800

    AuctionSniperEndToEndTest

---
+++ src/test/java/endtoend/auctionsniper/ApplicationRunner.java
@@ -0,0 +1,15 @@
+package endtoend.auctionsniper;
+
+public class ApplicationRunner {
+
+    public void startBiddingIn(FakeAuctionServer auction) {
+        
+    }
+
+    public void showSniperHasLostAuction() {
+        
+    }
+
+    public void stop() {
+    }
+}
---
+++ src/test/java/endtoend/auctionsniper/FakeAuctionServer.java
@@ -0,0 +1,25 @@
+package endtoend.auctionsniper;
+
+public class FakeAuctionServer {
+    private final String itemId;
+
+
+    public FakeAuctionServer(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public void startSellingItem() {
+        
+    }
+
+    public void hasReceivedJoinRequestFromSniper() {
+        
+    }
+
+    public void announceClosed() {
+        
+    }
+
+    public void stop() {
+    }
+}
