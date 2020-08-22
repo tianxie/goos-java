684a235676d32bca3d1fccc50f61a52d58356be2
tianxie <txie@live.cn>
2020-08-22 11:21:43 +0800

    Passing the first test

---
+++ src/main/java/auctionsniper/Main.java
@@ -0,0 +1,11 @@
+package auctionsniper;
+
+public class Main {
+
+    public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
+    public static final String SNIPER_STATUS_NAME = "";
+
+    public static void main(String xmppHostname, String sniperId, String sniperPassword, String itemId) {
+
+    }
+}
--- src/test/java/endtoend/auctionsniper/ApplicationRunner.java
+++ src/test/java/endtoend/auctionsniper/ApplicationRunner.java
@@ -1,15 +1,40 @@
 package endtoend.auctionsniper;
 
-public class ApplicationRunner {
+import auctionsniper.Main;
 
-    public void startBiddingIn(FakeAuctionServer auction) {
-        
+import static endtoend.auctionsniper.FakeAuctionServer.XMPP_HOSTNAME;
+
+public class ApplicationRunner {
+    public static final String SNIPER_ID = "sniper";
+    public static final String SNIPER_PASSWORD = "sniper";
+    private static final String STATUS_JOINING = "Joining";
+    private static final String STATUS_LOST = "Lost";
+    private AuctionSniperDriver driver;
+
+    public void startBiddingIn(final FakeAuctionServer auction) {
+        Thread thread = new Thread("Test Application") {
+            @Override
+            public void run() {
+                try {
+                    Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD, auction.getItemId());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.setDaemon(true);
+        thread.start();
+        driver = new AuctionSniperDriver(1000);
+        driver.showSniperStatus(STATUS_JOINING);
     }
 
     public void showSniperHasLostAuction() {
-        
+        driver.showSniperStatus(STATUS_LOST);
     }
 
     public void stop() {
+        if (driver != null) {
+            driver.dispose();
+        }
     }
 }
