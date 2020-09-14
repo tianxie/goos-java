06b8600d05512bcd7e019cac187007454d1df54a
tianxie <txie@live.cn>
2020-09-14 22:58:48 +0800

    Chapter 11

--- src/test/java/endtoend/auctionsniper/AuctionSniperDriver.java
+++ src/test/java/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -2,6 +2,7 @@ package endtoend.auctionsniper;
 
 
 import auctionsniper.Main;
+import auctionsniper.ui.MainWindow;
 import com.objogate.wl.swing.AWTEventQueueProber;
 import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JLabelDriver;
@@ -13,14 +14,14 @@ public class AuctionSniperDriver extends JFrameDriver {
     public AuctionSniperDriver(int timeoutMillis) {
         super(new GesturePerformer(),
                 JFrameDriver.topLevelFrame(
-                        named(Main.MAIN_WINDOW_NAME),
+                        named(MainWindow.MAIN_WINDOW_NAME),
                         showingOnScreen()),
                 new AWTEventQueueProber(timeoutMillis, 100));
     }
 
     public void showSniperStatus(String statusText) {
         new JLabelDriver(
-                this, named(Main.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
+                this, named(MainWindow.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
     }
 
     public void dispose() {
