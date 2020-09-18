315b0b2c1084cecd9322d677e83b51540a276517
tianxie <txie@live.cn>
2020-09-18 21:18:59 +0800

    Single item - join, lose without bidding

--- src/main/java/auctionsniper/Main.java
+++ src/main/java/auctionsniper/Main.java
@@ -20,6 +20,8 @@ public class Main {
     public static final String AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
 
     private MainWindow ui;
+    @SuppressWarnings("unused")
+    private Chat notToBeGCd;
 
     public Main() throws Exception {
         startUserInterface();
@@ -36,19 +38,28 @@ public class Main {
 
     public static void main(String... args) throws Exception {
         Main main = new Main();
-        XMPPConnection connection = connectTo(
-                args[ARG_HOSTNAME],
-                args[ARG_USERNAME],
-                args[ARG_PASSWORD]);
+        main.joinAuction(
+                connection(args[ARG_HOSTNAME], args[ARG_USERNAME], args[ARG_PASSWORD]),
+                args[ARG_ITEM_ID]);
+    }
+
+    private void joinAuction(XMPPConnection connection, String itemId) throws XMPPException {
         Chat chat = connection.getChatManager().createChat(
-                auctionId(args[ARG_ITEM_ID], connection),
+                auctionId(itemId, connection),
                 new MessageListener() {
                     @Override
                     public void processMessage(Chat chat, Message message) {
-                        // nothing yet
+                        SwingUtilities.invokeLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                ui.showStatus(MainWindow.STATUS_LOST);
+                            }
+                        });
                     }
                 }
         );
+        this.notToBeGCd = chat;
+
         chat.sendMessage(new Message());
     }
 
@@ -57,7 +68,7 @@ public class Main {
                 connection.getServiceName());
     }
 
-    private static XMPPConnection connectTo(String hostname, String username, String password) throws XMPPException {
+    private static XMPPConnection connection(String hostname, String username, String password) throws XMPPException {
         XMPPConnection connection = new XMPPConnection(hostname);
         connection.connect();
         connection.login(username, password, AUCTION_RESOURCE);
--- src/main/java/auctionsniper/ui/MainWindow.java
+++ src/main/java/auctionsniper/ui/MainWindow.java
@@ -7,7 +7,8 @@ import java.awt.*;
 public class MainWindow extends JFrame {
     public static final String SNIPER_STATUS_NAME = "sniper status";
     public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
-    private static final String STATUS_JOINING = "Joining";
+    public static final String STATUS_JOINING = "Joining";
+    public static final String STATUS_LOST = "Lost";
     private final JLabel sniperStatus = createLabel(STATUS_JOINING);
 
     public MainWindow() {
@@ -24,5 +25,9 @@ public class MainWindow extends JFrame {
         result.setName(SNIPER_STATUS_NAME);
         result.setBorder(new LineBorder(Color.BLACK));
         return result;
+    }
+
+    public void showStatus(String status) {
+        sniperStatus.setText(status);
     }
 }
